68ca753b7f1f4c768f0ab6016646b41fc1871626
akademiaEnvelo <kamil.wojkowski@ppuc.pl>
2022-12-22 08:13:58 +0100

    feat: pre material resources

---
+++ angular-material-playground/src/app/app.component.ts
@@ -0,0 +1,82 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+import { NonNullableFormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { startWith, pairwise } from 'rxjs';
+
+export interface JobOffer {
+  name: string;
+  description: {
+    name: string;
+    items: string[];
+  };
+  salary: number;
+  techList: {
+    name: string;
+    seniority: 'Entry' | 'Junior' | 'Mid' | 'Senior' | 'Expert';
+  }[];
+}
+
+export type JobDescription = JobOffer['description'];
+export type JobTechItem = JobOffer['techList'];
+export type JobTechSeniorityLevel = JobOffer['techList'][number]['seniority'];
+
+@Component({
+  selector: 'app-root',
+  template: ` test `,
+})
+export class AppComponent {
+  http = inject(HttpClient);
+  formBuilder = inject(NonNullableFormBuilder);
+
+  offers = this.http.get<(JobOffer & { id: number })[]>(
+    'http://localhost:3000/jobOffers'
+  );
+
+  form = this.formBuilder.group({
+    name: this.formBuilder.control(''),
+    description: this.formBuilder.group({
+      heading: this.formBuilder.control(''),
+      items: this.formBuilder.array<FormControl<string>>([]),
+    }),
+    salary: this.formBuilder.control(0),
+    techList: this.formBuilder.array<
+      FormGroup<{
+        name: FormControl<string>;
+        seniority: FormControl<JobTechSeniorityLevel>;
+      }>
+    >([]),
+  });
+
+  ngOnInit() {
+    this.form.controls.techList.valueChanges
+      .pipe(startWith([]), pairwise())
+      .subscribe(([old, current]) => {
+        if (old.length < current.length) {
+          return;
+        }
+
+        if (old.length > current.length) {
+          return;
+        }
+      });
+  }
+
+  createTechSkillForm(name: string) {
+    return this.formBuilder.group({
+      name: this.formBuilder.control(name),
+      seniority: this.formBuilder.control<JobTechSeniorityLevel>('Mid'),
+    });
+  }
+
+  addOffer() {
+    this.form.markAllAsTouched();
+
+    if (this.form.invalid) {
+      return;
+    }
+
+    this.http
+      .post<any>('http://localhost:3000/jobOffers', {})
+      .subscribe(() => {});
+  }
+}
---
+++ angular-material-playground/src/app/app.module.ts
@@ -0,0 +1,14 @@
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [BrowserModule, HttpClientModule, ReactiveFormsModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
